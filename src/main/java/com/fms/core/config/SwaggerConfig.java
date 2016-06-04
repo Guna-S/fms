db2c32fbc36332737430a100faf1dc1853de94ab
rmurugaian <rmurugaian@bridge2solutions.com>
2016-06-04 16:41:00 +0530

    swagger ui changes

--- src/main/java/com/fms/core/config/SwaggerConfig.java
+++ src/main/java/com/fms/core/config/SwaggerConfig.java
@@ -1,10 +1,10 @@
 package com.fms.core.config;
 
 import com.google.common.base.Predicate;
-import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -31,7 +31,7 @@ public class SwaggerConfig {
                 .version("0.1.0")
                 .build())
             .select()
-            .paths(Predicates.or(PATHS))
+            .paths(PathSelectors.any())
             .build();
     }
 }
